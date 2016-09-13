00fffc147e68b319a61d7bc45bb9858b51140e19
Romeno <RomenoEx@gmail.com>
2016-09-13 13:16:18 +0300

    echo require

--- www/echo.js
+++ www/echo.js
@@ -1,7 +1,8 @@
-window.echo = function(successCallback, errorCallback) {
-	cordova.exec(successCallback,
-				 errorCallback,
-				 "Echo",
-				 "echo",
-				 ["echo argument"]);
-}
+var argscheck = require('cordova/argscheck'),
+	utils = require('cordova/utils'),
+	exec = require('cordova/exec'),
+	channel = require('cordova/channel');
+	
+module.exports = function(successCallback, errorCallback) {
+	exec(successCallback, errorCallback, "Echo", "echo", ["dsdsdsdsds"]);
+};
