d1432480c741df58886802866741ae274f4e35d5
Romeno <RomenoEx@gmail.com>
2016-09-13 12:08:49 +0300

    package error inject

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -1,7 +1,9 @@
-package by.romeno.cordova.plugin;
+package by.romeno.cordova.plugin1;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
